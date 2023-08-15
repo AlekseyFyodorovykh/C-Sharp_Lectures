8c70069635927013401bb578ec9517b62c156121
ALEX <ALEX@example.com>
2023-08-15 16:29:37 +0300

    Create programm

---
+++ Example03/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array = {1, 15, 6, 46, 4, 9616, 64, 6, 46, 46, 1};
+
+int n = array.Length;
+
+int index = 0;
+int find = 6;
+
+while (index < n)
+{
+   if(array[index]==find)
+   {
+    Console.WriteLine(index);
+    break;
+   }
+   index++;
+}
+
+
